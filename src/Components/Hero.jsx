36fb1076270b43e907f784f1f2322a1c7811b34d
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 15:30:21 +0545

    done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Progress from './Progress';
 
 const Hero = () => {
   return (
-  <section className='h-[150vh] w-[100%] bg-[#000000]  flex flex-col items-center gap-10'>
+  <section className='h-[150vh] w-[100%] bg-[#1A1A1A]  flex flex-col items-center gap-10'>
 <div className=''>This is radial gradient 1</div>
 <h1>This is radial gradienr 2 </h1>
 <Progress/>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const Sidebar = () => {
   return (
    <>
-    <aside className='h-[100vh] fixed top-0 left-0 w-[230px] bg-[#9B9B9B] opacity-30'>
+    <aside className='h-[100vh] fixed top-0 left-0 w-[230px] bg-[#1e272e]'>
         <nav>
-            <ul>
+        <section>
+
+        </section>
+            <ul className='font-poppins font-medium text-white text-center'>
+
                 <li>Dashboard</li>
-                <li>Employees</li>\
-                <li></li>
+                <li>Employees</li>
+                <li>Engagement Rate </li>
+                <li>Tasks</li>
             </ul>
         </nav>
     </aside>
