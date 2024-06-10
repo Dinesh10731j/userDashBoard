819ee70fc4ee195b492f1bd87fbcbfc82e4e2a5e
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 17:21:11 +0545

    done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,26 +6,30 @@ const Sidebar = () => {
     <>
       <aside className="h-[100vh] fixed top-0 left-0 w-[230px] bg-[#2C2D2D] py-3">
         <nav>
-          <section className="flex justify-center items-center">
+          <section className="flex justify-center items-center gap-3">
             <section>
               <img src={User} alt="user" />
+            </section>
+            <section>
+                <h1 className="text-white font-poppins font-medium text-xl">Ashim Kafle</h1>
+                <h4 className="text-white font-poppins ">UI/UX Designer</h4>
             </section>
           </section>
           <ul className="font-poppins font-medium text-white text-center px-2 py-3 ">
           
           
-          <li className="hover:bg-cyan-100 px-3 py-4 ml-3 text-[#B070D0]">
+          <li className=" px-3 py-2 ml-3 text-[#B070D0]">
               Dashboard
               </li>
        
           
-            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">
+            <li className=" px-3 py-2 ml-3">
              Employees
             </li>
-            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">
+            <li className=" px-3 py-2 ml-3">
               Engagement Rate
             </li>
-            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">Tasks</li>
+            <li className=" px-3 py-2 ml-3">Tasks</li>
           </ul>
         </nav>
       </aside>
