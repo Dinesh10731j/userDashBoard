85f803d368fe99d5e7a33fa2a4bba4f5f8912b37
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 13:31:53 +0545

    done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = () => {
   return (
    <>
-    <aside className='h-[100vh] fixed top-0 left-0 w-[30px] shadow-xl'>
+    <aside className='h-[100vh] fixed top-0 left-0 w-[274px] bg-[#9B9B9B]'>
         <nav>
             <ul>
                 <li>Home</li>
