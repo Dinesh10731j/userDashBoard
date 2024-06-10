0feb945a73b174bee879c922455a43df3749717e
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 19:32:33 +0545

    done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,62 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "../assets/user.png";
-import dashboardIcon from "../assets/image 2.png";
-import EmployessIcon from "../assets/image 3.png";
-import EngagementRateIcon from "../assets/image 4.png";
-import TaskIcon from "../assets/image 6.png"
+import { Navdata } from "../Navdata/data";
+
 
 const Sidebar = () => {
+    const [color,setColor] = useState(false);
+    const handleColor=()=>{
+        setColor(true);
+    }
   return (
-    <>
-      <aside className="h-[100vh] fixed top-0 left-0 w-[230px] bg-[#2C2D2D] py-3">
-        <nav>
-          <section className="flex justify-center items-center gap-3">
-            <section>
-              <img src={User} alt="user" />
-            </section>
-            <section>
-                <h1 className="text-white font-poppins font-medium text-xl">Ashim Kafle</h1>
-                <h4 className="text-white font-poppins ">UI/UX Designer</h4>
-            </section>
-          </section>
-          <ul className="font-poppins font-medium text-white text-center px-2 py-3 flex flex-col gap-2 ">
-          <section className="flex flex-row gap-3  justify-center">
-            <section  className="h-[34px] w-[34px]">
-                <img src={dashboardIcon} alt="icon"/>
-            </section>
-            <section>
-                <li>Dashboard</li>
-            </section>
-          </section>
-          <section className="flex flex-row gap-3 items-center justify-center b">
-            <section  className="h-[34px] w-[34px]">
-                <img src={EmployessIcon} alt="icon"/>
-            </section>
-            <section>
-                <li>Employees</li>
-            </section>
-          </section>
-          <section className="flex flex-row gap-3  justify-center">
-            <section className="h-[34px] w-[34px]">
-                <img src={EngagementRateIcon} alt="icon"/>
-            </section>
-            <section>
-                <li>Engagement Rate </li>
-            </section>
-          </section>
-          <section className="flex flex-row gap-3  justify-center">
-            <section className="h-[34px] w-[34px]">
-                <img src={TaskIcon} alt="icon"/>
-            </section>
-            <section>
-            <li>Task</li>
-            </section>
-          </section>
-          
-         </ul>
-        </nav>
-      </aside>
-    </>
+    <aside className="h-screen fixed top-0 left-0 w-[230px] bg-[#2C2D2D] py-3">
+      <nav className="flex flex-col">
+        <section className="flex items-center justify-center mb-6">
+          <img src={User} alt="user" className="w-16 h-16 rounded-full" />
+          <div className="ml-3">
+            <h1 className="text-white font-medium text-lg">Ashim Kafle</h1>
+            <h4 className="text-white">UI/UX Designer</h4>
+          </div>
+        </section>
+        <ul className="font-medium flex flex-col gap-3 px-3">
+          {Navdata.map((item, index) => (
+            <li
+              key={index}
+              className={`flex items-center gap-3 p-2 text-white transition-all delay-150`}
+             
+
+              
+            >
+              <img src={item.icon} alt={item.text} className="w-6 h-7" />
+              <span>{item.text}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
   );
 };
 
---
+++ src/Navdata/data.js
@@ -0,0 +1,19 @@
+export const Navdata = [
+    {
+    "text":"Dashboard",
+    "icon":'../src/assets/image 2.png'
+},
+{
+    "text":"Employees",
+    "icon":'../src/assets/image 3.png'
+},
+{
+    "text":"Engagement Rate",
+    "icon":'../src/assets/image 4.png'
+},
+{
+    "text":"Task",
+    "icon":'../src/assets/image 6.png'
+},
+
+]
