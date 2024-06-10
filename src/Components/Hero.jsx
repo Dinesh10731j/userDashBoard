013a120df984b05097794a1cf18a0548b879090c
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 17:15:23 +0545

    done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,10 +8,7 @@ const Hero = () => {
 <section className='h-[120px] w-[120px] rounded-full bg-orange-400 blur-3xl -z-6 ml-40 fixed bottom-12'></section>
 <section className='h-[120px] w-[120px] rounded-full bg-green-400 blur-3xl -z-6 fixed top-60 right-60' ></section>
 <section className='h-[120px] w-[120px] rounded-full bg-cyan-500 blur-3xl -z-6 fixed top-20'></section>
-
-
 <Progress/>
-
   </section>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
+import User from "../assets/user.png";
 
 const Sidebar = () => {
   return (
-   <>
-    <aside className='h-[100vh] fixed top-0 left-0 w-[230px] bg-[#1e272e]'>
+    <>
+      <aside className="h-[100vh] fixed top-0 left-0 w-[230px] bg-[#2C2D2D] py-3">
         <nav>
-        <section>
-
-        </section>
-            <ul className='font-poppins font-medium text-white text-center'>
-
-                <li>Dashboard</li>
-                <li>Employees</li>
-                <li>Engagement Rate </li>
-                <li>Tasks</li>
-            </ul>
+          <section className="flex justify-center items-center">
+            <section>
+              <img src={User} alt="user" />
+            </section>
+          </section>
+          <ul className="font-poppins font-medium text-white text-center px-2 py-3 ">
+          
+          
+          <li className="hover:bg-cyan-100 px-3 py-4 ml-3 text-[#B070D0]">
+              Dashboard
+              </li>
+       
+          
+            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">
+             Employees
+            </li>
+            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">
+              Engagement Rate
+            </li>
+            <li className="hover:bg-cyan-100 px-3 py-4 ml-3">Tasks</li>
+          </ul>
         </nav>
-    </aside>
-   </>
-  )
-}
+      </aside>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
