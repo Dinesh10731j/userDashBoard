92aad0c9de59e89462233b21195f85eb5eff4f17
Dinesh10731j <dinesh1122@gmail.com>
2024-06-10 14:55:39 +0545

    done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Progress from './Progress';
 
 const Hero = () => {
   return (
-  <section className='h-[100vh] w-[100%] bg-[#000000] -z-2 flex flex-col items-center'>
+  <section className='h-[150vh] w-[100%] bg-[#000000]  flex flex-col items-center gap-10'>
 <div className=''>This is radial gradient 1</div>
 <h1>This is radial gradienr 2 </h1>
 <Progress/>
--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import Bargraph from "../assets/Bargraph.png"
+import Bargraph from "../assets/Bargraph.png";
+import Piechart from "../assets/Piechart.png";
+import Report from "../assets/Report.png";
+import Calender from "../assets/Calender.png";
 const Progress = () => {
   return (
    <>
-    <section className='h-[263px] w-[1000px] ml-40'>
-    <img src={Bargraph} className='absolute'/>
+   <section className='h-[982px] w-[1007px] flex flex-col justify-center items-center gap-6 '>
+   <section className='h-[263px] w-[1002px] ml-40 relative'>
+    <img src={Bargraph} alt='Bargraph' className='absolute'/>
 
     </section>
+    <section className='flex flex-col  ml-40 mt-7 w-[1002px] h-[581px]'>
+<section className='flex flex-row '>
+<section className='text-white'>
 
-<section className='flex flex-col flex-wrap  justify-center items-center'>
-    <section className='text-white'>This is section 1 </section>
-    <section className='text-white'>Thi is sectopn 2</section>
+    <img src={Piechart}/>
+</section>
+<section className='flex flex-col gap-3 '>
+    <section  className="relative h-[283px] w-[256px] ml-12">
+        <img src={Calender} alt="Calender" className='absolute'/>
+    </section>
+    <section className='relative h-[283px] w-[256px] ml-12'>
+        <img src={Report} alt='Report' className='absolute' />
+    </section>
 </section>
 
+</section>
+    </section>
+    </section>
    </>
   )
 }
